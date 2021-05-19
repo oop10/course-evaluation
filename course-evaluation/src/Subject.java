fcfb842b28a5aad931701f8b6dcc9ec4c0f67b42
JAEHOON <jaehoonlee@gc.gachon.ac.kr>
2021-05-19 18:08:08 +0900

    Create Subject Class

---
+++ course-evaluation/src/Subject.java
@@ -0,0 +1,72 @@
+
+/** 
+ * Subject
+ * This class means each subject.
+ * (ex: Object-Oriented Programming, Data Structure, Probability and Statistics, ...)
+ * 
+ * This class can get average evaluation score, print every evaluation, print all class,
+ * and store subject code, subject title, and reference of every classes.
+ * 
+ * @author Lee Jaehoon
+ * @version 1.0
+ * @since 2021-05-19
+ */
+
+public class Subject {
+	
+	private String code;	// Subject code (ex. 09805)
+	private String title;	// Subject title (ex. Object-Oriented Programming)
+	private Class[] classes;
+	
+	public double getAverage() {
+		double score = 0;
+		
+		// Get sum of every class's average evaluation score.
+		for(Class elem: this.classes) {
+			score += elem.getAverage();
+		}
+		score /= this.classes.length;
+		
+		return score;
+	}
+	
+	public void printAllEvaluation()	{
+		// Print every evaluation in classes.
+		for(Class elem: this.classes) {
+			// Print every evaluation in given class.
+			elem.printAllEvalution();
+		}
+	}
+	
+	public void printAllClass()	{
+		// Print information about every class.
+		for(Class elem: this.classes) {
+			// Print information about given class.
+			elem.printInformation();
+		}
+	}
+	
+	// Accessor methods
+	public String getCode() {
+		return this.code;
+	}
+	
+	public String getTitle()	{
+		return this.title;
+	}
+	
+	// Mutator methods	
+	public void setCode(String code)	{
+		this.code = code;
+	}
+	
+	public void setTitle(String title)	{
+		this.title = title;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
