c0cdaf202d58be06d3b92747b51d68c46da9d813
leeeunseo912 <80867941+leeeunseo912@users.noreply.github.com>
2021-05-25 22:27:05 +0900

    Create Evaluation.java
    evaluation

---
+++ course-evaluation/src/Evaluation.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Evaluation {
+	
+	static int[] scores = new int[5];
+	
+//	public int setEvalution() {
+//		
+//	}
+//	
+//	public int getEvalution() {
+//		
+//	}
+	
+	
+
+	public static void main(String[] args) {
+		
+	//	Student student = new Student();
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("This program is to evalute the your course.\n");
+		
+		
+		System.out.printf("Question 1 : 강의 내용이 강의 계획서대로 진행되었다 (1~5) ");
+		scores[0]=keyboard.nextInt();
+		while(scores[0] <= 0 || scores[0] > 5) {
+			System.out.printf("Question 1 : 강의 내용이 강의 계획서대로 진행되었다 (1~5) ");
+			scores[0]=keyboard.nextInt();
+		}
+		
+		System.out.printf("Question 2 : 수업을 위한 적절한 교육매체가 준비되었다 (1~5) ");
+		scores[1]=keyboard.nextInt();
+		while(scores[1] <= 0 || scores[1] > 5) {
+			System.out.printf("Question 2 : 수업을 위한 적절한 교육매체가 준비되었다 (1~5) ");
+			scores[1]=keyboard.nextInt();
+		}
+		
+		System.out.printf("Question 3 : 강의 진행은 체계적이었다. (1~5) ");
+		scores[2]=keyboard.nextInt();
+		while(scores[2] <= 0 || scores[2] > 5) {
+			System.out.printf("Question 3 : 강의 진행은 체계적이었다. (1~5) ");
+			scores[2]=keyboard.nextInt();
+		}
+		
+		System.out.printf("Question 4 : 나는 이 수업을 통하여 많은 지식을 얻었다 (1~5) ");
+		scores[3]=keyboard.nextInt();
+		while(scores[3] <= 0 || scores[3] > 5) {
+			System.out.printf("Question 4 : 나는 이 수업을 통하여 많은 지식을 얻었다 (1~5) ");
+			scores[3]=keyboard.nextInt();
+		}
+		
+		System.out.printf("Question 5 : 나는 이 수업을 다른 학생에게 추천하고 싶다 (1~5) ");
+		scores[4]=keyboard.nextInt();
+		while(scores[4] <= 0 || scores[4] > 5) {
+			System.out.printf("Question 5 : 나는 이 수업을 다른 학생에게 추천하고 싶다 (1~5) ");
+			scores[4]=keyboard.nextInt();
+		
+		}
+		System.out.println("Check your evaluation score : " + scores[0] +" " + scores[1] + " " + scores[2] + " " + scores[3] + " " + scores[4]);
+	}
+}	
