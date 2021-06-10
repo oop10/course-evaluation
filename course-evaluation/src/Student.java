f2140ae5ee7945b2a4035287b76efd8b526dddbc
bugyeong0705 <kbg010705@naver.com>
2021-06-11 03:10:00 +0900

    rearrange demo,student

--- course-evaluation/src/Student.java
+++ course-evaluation/src/Student.java
@@ -1,4 +1,3 @@
-import java.io.*;
 
 public class Student extends Person{
 
@@ -6,7 +5,7 @@ public class Student extends Person{
 	private int classCount = 0;
 	private String password;
 	private Evaluation[] evaluate = new Evaluation[maxClasses];
-	private Class[] classes = new Class[maxClasses];
+	private Classes[] classes = new Classes[maxClasses];
 	
 	//Constructor
 	public Student(String name,String id,String password) {
@@ -15,7 +14,7 @@ public class Student extends Person{
 	}
 	
 	//Method
-	public void appendClasses(Class newClass) {
+	public void appendClasses(Classes newClass) {
 		classes[classCount] = newClass;
 		classCount++;
 	}
@@ -26,49 +25,23 @@ public class Student extends Person{
 		classes[selectSubject].appendEvaluation(evaluate[selectSubject]);
 	}
 	
-	@Override
-	public boolean canLogin() throws IOException {
-		BufferedReader inputStream = null;
-		int j = 0;
-		try {
-			inputStream = new BufferedReader(new FileReader("information.txt"));
-			String line = null;
-			line = inputStream.readLine();
-			while((line = inputStream.readLine()) != null) {
-				String[] ary = line.split(",");
-				for(int i = 3; i < ary.length; i++) {
-					if(id.equals(ary[i])) {
-						subject[j] = ary[0] + "-" + ary[1];
-						j++;
-					}
-				}
-			}
-		}
-		catch(Exception e) {
-			System.out.println("exception!");
-			System.exit(0);
-		}
-		if(inputStream != null) {
-			inputStream.close();
-		}
-		
-		if(j == 0) {
-			return false;
-		}
-		else {
-			System.out.println("**login success**");
-			System.out.println("**subject list you can evaluate**");
-			for(int i = 0; subject[i] != null; i++) {
-				System.out.println(i + "." + subject[i]);
-				subjectLength++;
-			}
-			return true;
-		}
+	public void printAllClassesName() {
+		for(int i = 0; i < classCount; i++) {
+			System.out.println(i + ":" + classes[i].getTitle());
+		}		
 	}
+
+	//Mutator
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	//Accessor
 	public String getPassword() {
 		return password;
 	}
+	public String getSpecificClassesName(int index) {
+		return classes[index].getTitle();
+	}
+
 }
